546c1af7a0917901d9b96477f33dcd320a88e1c3
vedantswain <vedant12118@iiitd.ac.in>
2015-10-28 10:58:33 +0530

    setup placeholders for chart rendering in views

--- snooknows/you/static/you/js/m_graph.js
+++ snooknows/you/static/you/js/m_graph.js
@@ -19,8 +19,10 @@ function renderDonutGraph(div_id,dat){
 	        color: "#FDB45C",
 	        highlight: "#FFC870",
 	        label: "Yellow"
-	    }
+	    },
 	]
+
+	// data=dat
 
 	var options={
 	    //Boolean - Whether we should show a stroke on each segment
@@ -53,6 +55,66 @@ function renderDonutGraph(div_id,dat){
 	}
 
 	var myChart = new Chart(ctx).Doughnut(data,options);
+	// var legend = myChart.generateLegend();
+	// document.getElementById("subredLegend").innerHTML = legend;
+}
+
+function renderBarGraph(div_id,dat,labs){
+	var ctx = document.getElementById(div_id).getContext("2d");
+
+	var data = {
+    labels: ['January', "February", "March", "April", "May", "June", "July"],
+    // labels: labs,
+    datasets: [
+        {
+            label: "My First dataset",
+            fillColor: "#720733",
+            strokeColor: "rgba(220,220,220,0.8)",
+            highlightFill: "#AB0C4D",
+            highlightStroke: "rgba(220,220,220,1)",
+            data: [65, 59, 80, 81, 56, 55, 40],
+            // data: dat
+        }
+        ]
+	};
+
+	var options={
+	    //Boolean - Whether the scale should start at zero, or an order of magnitude down from the lowest value
+	    scaleBeginAtZero : true,
+
+	    //Boolean - Whether grid lines are shown across the chart
+	    scaleShowGridLines : true,
+
+	    //String - Colour of the grid lines
+	    scaleGridLineColor : "rgba(0,0,0,.05)",
+
+	    //Number - Width of the grid lines
+	    scaleGridLineWidth : 1,
+
+	    //Boolean - Whether to show horizontal lines (except X axis)
+	    scaleShowHorizontalLines: false,
+
+	    //Boolean - Whether to show vertical lines (except Y axis)
+	    scaleShowVerticalLines: false,
+
+	    //Boolean - If there is a stroke on each bar
+	    barShowStroke : true,
+
+	    //Number - Pixel width of the bar stroke
+	    barStrokeWidth : 1,
+
+	    //Number - Spacing between each of the X value sets
+	    barValueSpacing : 10,
+
+	    //Number - Spacing between data sets within X values
+	    barDatasetSpacing : 30,
+
+	    //String - A legend template
+	    legendTemplate : "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<datasets.length; i++){%><li><span style=\"background-color:<%=datasets[i].fillColor%>\"></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>"
+
+	}
+
+	var myChart = new Chart(ctx).Bar(data,options);
 	// var legend = myChart.generateLegend();
 	// document.getElementById("subredLegend").innerHTML = legend;
 }
--- snooknows/you/views.py
+++ snooknows/you/views.py
@@ -20,23 +20,58 @@ def analyse(request,username):
 	max_score=98
 	abs_score=41
 
+	subredData=getSegments({})
+
+	(comboData,comboLabels)=getBars({})
+
 	context = RequestContext(request, {
 		'username':username,
 		'reddituser': reddit_user,
 		'vul_score': vul_score,
 		'gradient_1': grad[0],
 		'gradient_2': grad[1],
-		'abs_score' : str(abs_score)+"/"+str(max_score)
+		'abs_score' : str(abs_score)+"/"+str(max_score),
+		'subred_data':subredData,
+		'combo_data':comboData,
+		'combo_labels':comboLabels,
 		})
 
 	return HttpResponse(template.render(context))
 
 def getSegments(subredWeights):
 	colours=["#15507a","#5eb1ba","#6ED1DB","#720733","#c9c6b5","#d10061","#e67162","#db601e","#C1E66E","#317564"]
-	hilites=["#1F78B8","6BCCD6","A30B4A","AB0C4D","E8E5D3","E8006C","FC7D6D","FA6E23","43A38B","317564"]
+	hilites=["#1F78B8","#6BCCD6","#A30B4A","#AB0C4D","#E8E5D3","#E8006C","#FC7D6D","#FA6E23","#43A38B","#317564"]
 
-	
-	
+	i=0
+	data_str="[\n"
+	for key in subredWeights.keys():
+		datum="{\n"
+		datum+="value : "+str(subredWeights(key))+",\n"
+		datum+="color : '"+colours[i]+"',\n"
+		datum+="highlight : '"+hilites[i]+"',\n"
+		datum+="label : '"+key+"',\n"
+		datum+="},\n"
+		count+=1
+		data_str+datum
+
+	data_str+="]"
+
+	return data_str
+
+def getBars(comboDict):
+	labels_str="["
+	data_str="["
+
+	for key in comboDict.keys():
+		labels_str+="'"+key+"',"
+		data_str+=str(comboDict(key))+","
+
+	labels_str+="]"
+	data_str+="]"
+
+	return data_str,labels_str
+
+
 def getColour(score):
 	gradient = {} 
 	gradient["low"]=["#27ae60","#16a085"]
